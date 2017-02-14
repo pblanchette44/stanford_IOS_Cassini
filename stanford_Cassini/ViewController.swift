cfb955acdf483cb4ed22f754d36dccb567f6d220
Phil <pblanchette44@hotmail.com>
2017-02-14 12:06:28 -0500

    Initial Commit

---
+++ stanford_Cassini/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  stanford_Cassini
+//
+//  Created by Philippe Blanchette on 2017-02-14.
+//  Copyright © 2017 Philippe Blanchette. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ stanford_CassiniTests/stanford_CassiniTests.swift
@@ -0,0 +1,36 @@
+//
+//  stanford_CassiniTests.swift
+//  stanford_CassiniTests
+//
+//  Created by Philippe Blanchette on 2017-02-14.
+//  Copyright © 2017 Philippe Blanchette. All rights reserved.
+//
+
+import XCTest
+@testable import stanford_Cassini
+
+class stanford_CassiniTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
